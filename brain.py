b35b0c05e30dcebc901737baf6ae496d1008bebb
Simon Serrano <simon.serrano@hotmail.fr>
2019-06-20 23:15:14 +0200

    Working methods to detect props

--- brain.py
+++ brain.py
@@ -1,9 +1,7 @@
-from keras.models import Model
-from keras.layers import Dense, Input, Concatenate, Lambda
-from keras.optimizers import RMSprop
 import random
 import cv2
 import numpy as np
+import math
 
 
 class NeuralNetwork():
@@ -16,20 +14,11 @@ class NeuralNetwork():
 
 
     def create_baby(self):
-        inputTensor = Input(self.input_shape)
-        connect = random.randint(0,4)
-        edge = Lambda(lambda x: x[:, connect:connect+1])(inputTensor)
-        outputTensor = Dense(1, activation='relu', 
-            kernel_initializer='random_uniform')(edge)
-        self.model = Model(inputTensor, outputTensor)
-        self.model.compile(loss='mse',
-                      optimizer=RMSprop(),
-                      metrics=['accuracy'])
-        self.model.summary()
+        
         
         
 
-    def detect_props(self, state):
+    def detect_props(self, state, last_bp):
         
         def remove_background(state):
             state[state == 144] = 0
@@ -62,10 +51,31 @@ class NeuralNetwork():
                 tl, tr, bl, br = corners[0], corners[1], corners[2], corners[3]
                 return ((br[0,0]+tl[0,0])/2, (br[0,1]+tl[0,1])/2)
 
+        def ball_velocity(last_bp, bp):
+            bv = 0
+            if bp is not None and last_bp is not None:
+                bv = math.sqrt(math.pow(bp[0]-last_bp[0],2)+math.pow(bp[1]-last_bp[1], 2))
+            return bv
+
+        def enemy_position(state):
+            state = state[:, 16:17]
+            top, bottom = None, None
+            for i in range(160):
+                if state[i][0][0] == 213 and top is None:
+                    top = i
+                elif (state[i][0][0] == 0 or i == 159) and top is not None:
+                    bottom = i-1
+                    break
+            position = (top+bottom)/2
+            return position
+
+
         state = crop(state)
         state = remove_background(state)
         pp = paddle_position(state)
         bp = ball_position(state)
+        bv = ball_velocity(last_bp, bp)
+        return bp, pp, bv, ep
     
         
            
--- temp.py
+++ temp.py
@@ -3,38 +3,69 @@ import sys
 import numpy
 import cv2
 import numpy as np
+import math
 
-def remove_background(state):
-    state[state == 144] = 0
-    state[state == 72] = 0
-    state[state == 17] = 0
-    return state
+def detect_props(state, last_bp):
         
-def crop(state):
-    state = state[34:193]
-    return state
+        def remove_background(state):
+            state[state == 144] = 0
+            state[state == 72] = 0
+            state[state == 17] = 0
+            return state
+        
+        def crop(state):
+            state = state[33:194]
+            return state
+        
+        def paddle_position(state):
+            state = state[:, 140:141]
+            top, bottom = None, None
+            for i in range(160):
+                if state[i][0][1] == 186 and top is None:
+                    top = i
+                elif (state[i][0][1] == 0 or i == 159) and top is not None:
+                    bottom = i-1
+                    break
+            position = (top+bottom)/2
+            return position
+        
+        def ball_position(state):
+            state = state[:, 40:140]
+            gray_state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
+            gray_state = np.float32(gray_state)
+            corners = cv2.goodFeaturesToTrack(gray_state, 4, 0.01, 0)
+            if(corners is not None and corners.size ==8) :
+                tl, tr, bl, br = corners[0], corners[1], corners[2], corners[3]
+                return ((br[0,0]+tl[0,0])/2, (br[0,1]+tl[0,1])/2)
 
-def paddle_position(state):
-    state = state[:, 140:141]
-    top, bottom = None, None
-    for i in range(158):
-        if state[i][0][1] == 186 and top is None:
-            top = i
-        elif (state[i][0][1] == 0 or i == 159) and top is not None:
-            bottom = i-1
-            break
-    position = (top+bottom)/2
-    return position
+        def ball_velocity(last_bp, bp):
+            bv = 0
+            if bp is not None and last_bp is not None:
+                bv = math.sqrt(math.pow(bp[0]-last_bp[0],2)+math.pow(bp[1]-last_bp[1], 2))
+            return bv
 
-def ball_position(state):
-    state = state[:, 40:140]
-    gray_state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
-    gray_state = np.float32(gray_state)
-    corners = cv2.goodFeaturesToTrack(gray_state, 4, 0.01, 0)
-    if(corners is not None and corners.size ==8) :
-        tl, tr, bl, br = corners[0], corners[1], corners[2], corners[3]
-        return ((br[0,0]+tl[0,0])/2, (br[0,1]+tl[0,1])/2)
+        def enemy_position(state):
+            state = state[:, 16:17]
+            top, bottom = None, None
+            for i in range(160):
+                if state[i][0][0] == 213 and top is None:
+                    top = i
+                elif (state[i][0][0] == 0 or i == 159) and top is not None:
+                    bottom = i-1
+                    break
+            if(top is not None and bottom is not None):
+                position = (top+bottom)/2
+                return position
+            return None
 
+
+        state = crop(state)
+        state = remove_background(state)
+        pp = paddle_position(state)
+        bp = ball_position(state)
+        bv = ball_velocity(last_bp, bp)
+        ep = enemy_position(state)
+        return bp, pp, bv, ep
 
 if __name__ == "__main__":
     env = gym.make('Pong-v0')
@@ -43,19 +74,7 @@ if __name__ == "__main__":
         env.render()
         state, reward, done,  _ = env.step(2)
         np.set_printoptions(threshold=sys.maxsize)
-        state = crop(state)
-        state = remove_background(state)
-        ball_position(state)
-        printed = set()
-        for i in range (158):
-            for j in range(160):
-                for k in range(3):
-                    if(state[i, j, k] != 0 and i not in printed):
-                        pass
-                        #print(i)
-                        #printed.add(i)
-                
-        print(paddle_position(state), "- paddle pos")
+        print(detect_props(state, list([0, 0])))
 
 
     
